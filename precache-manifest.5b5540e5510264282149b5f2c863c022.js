262caa26f42cc503526a52b6c38ef05cf1daed1e
alexanderjaramillo <alexunjm@gmail.com>
2020-05-13 00:53:52 -0500

    update site to react project sample

---
+++ precache-manifest.5b5540e5510264282149b5f2c863c022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bb51956d8c5a51a9045ffa72e464f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b99cebd04780df9c7b",
+    "url": "/static/css/main.baa53473.chunk.css"
+  },
+  {
+    "revision": "4c44d2ab51ff9643ed25",
+    "url": "/static/js/2.8ec57da6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ec57da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b99cebd04780df9c7b",
+    "url": "/static/js/main.12752688.chunk.js"
+  },
+  {
+    "revision": "ba54e7551e524c23a393",
+    "url": "/static/js/runtime-main.e2ab80d1.js"
+  },
+  {
+    "revision": "db89bb5ceab87f9c0fcc2ab36c189c2c",
+    "url": "/static/media/lorem-ipsum.db89bb5c.txt"
+  }
+]);
